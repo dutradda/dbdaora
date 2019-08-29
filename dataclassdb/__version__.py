d4bc23c02b96466d548539e4f6ef9a5ae141a41a
Diogo Dutra <diogodutradamata@gmail.com>
2019-08-28 22:48:43 -0300

    Create __version__.py

---
+++ dataclassdb/__version__.py
@@ -0,0 +1,13 @@
+__title__ = 'dataclassdb'
+
+__version__ = '0.0.1'
+
+__description__ = 'Generates SQL and NoSQL Database Models from @dataclass'
+
+__author__ = 'Diogo Dutra'
+
+__author_email__ = 'diogodutradamata@gmail.com'
+
+__url__ = 'https://github.com/dutradda/dataclassdb'
+
+__license__ = 'MIT'
