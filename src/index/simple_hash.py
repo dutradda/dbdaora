d75310f15ac78e8d4018579749fb37cdf146cc7d
Diogo Dutra <diogo.albuquerque@luizalabs.com>
2020-04-24 20:37:23 -0300

    Deployed 95a64af with MkDocs version: 1.1

---
+++ src/index/simple_hash.py
@@ -0,0 +1,37 @@
+import asyncio
+from dataclasses import dataclass
+
+from dbdaora import (
+    DictFallbackDataSource,
+    DictMemoryDataSource,
+    HashRepository,
+)
+
+
+@dataclass
+class Person:
+    id: str
+    name: str
+    age: int
+
+
+def make_person(name: str, age: int) -> Person:
+    return Person(name.replace(' ', '_').lower(), name, age)
+
+
+class PersonRepository(HashRepository[str]):
+    entity_name = 'person'
+    entity_cls = Person
+    key_attrs = ('id',)
+
+
+repository = PersonRepository(
+    memory_data_source=DictMemoryDataSource(),
+    fallback_data_source=DictFallbackDataSource(),
+    expire_time=60,
+)
+person = make_person('John Doe', 33)
+asyncio.run(repository.add(person))
+
+geted_person = asyncio.run(repository.query(person.id).entity)
+print(geted_person)
---
+++ src/index/simple_service.py
@@ -0,0 +1,49 @@
+import asyncio
+from dataclasses import dataclass
+
+from dbdaora import (
+    DictFallbackDataSource,
+    DictMemoryDataSource,
+    HashRepository,
+    make_hash_service,
+)
+
+
+@dataclass
+class Person:
+    id: str
+    name: str
+    age: int
+
+
+def make_person(name: str, age: int) -> Person:
+    return Person(name.replace(' ', '_').lower(), name, age)
+
+
+class PersonRepository(HashRepository[str]):
+    entity_name = 'person'
+    entity_cls = Person
+    key_attrs = ('id',)
+
+
+async def make_memory_data_source() -> DictMemoryDataSource:
+    return DictMemoryDataSource()
+
+
+async def make_fallback_data_source() -> DictFallbackDataSource:
+    return DictFallbackDataSource()
+
+
+service = asyncio.run(
+    make_hash_service(
+        PersonRepository,
+        memory_data_source_factory=make_memory_data_source,
+        fallback_data_source_factory=make_fallback_data_source,
+        repository_expire_time=60,
+    )
+)
+person = make_person('John Doe', 33)
+asyncio.run(service.add(person))
+
+geted_person = asyncio.run(service.get_one(person.id))
+print(geted_person)
