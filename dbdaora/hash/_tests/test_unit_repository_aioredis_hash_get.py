be0840a1d4a900b1bf2ed0732f23bb401c0d414a
Diogo Dutra <diogo.albuquerque@luizalabs.com>
2020-04-25 02:48:00 -0300

    Improve tests

--- dbdaora/hash/_tests/test_unit_repository_aioredis_hash_get.py
+++ dbdaora/hash/_tests/test_unit_repository_aioredis_hash_get.py
@@ -59,6 +59,43 @@ async def test_should_raise_not_found_error_when_already_raised_before(
 
 
 @pytest.mark.asyncio
+async def test_should_set_already_not_found_error(
+    repository, mocker
+):
+    fake_entity = 'fake'
+    expected_query = HashQuery(repository, memory=True, id=fake_entity)
+    repository.memory_data_source.hgetall = asynctest.CoroutineMock(
+        side_effect=[None]
+    )
+    repository.memory_data_source.exists = asynctest.CoroutineMock(
+        side_effect=[False]
+    )
+    repository.fallback_data_source.get = asynctest.CoroutineMock(
+        return_value=None
+    )
+    repository.memory_data_source.set = asynctest.CoroutineMock()
+    repository.memory_data_source.hmset = asynctest.CoroutineMock()
+
+    with pytest.raises(EntityNotFoundError) as exc_info:
+        await repository.query(fake_entity).entity
+
+    assert exc_info.value.args == (expected_query,)
+    assert repository.memory_data_source.hgetall.call_args_list == [
+        mocker.call('fake:fake'),
+    ]
+    assert repository.memory_data_source.exists.call_args_list == [
+        mocker.call('fake:not-found:fake')
+    ]
+    assert repository.fallback_data_source.get.call_args_list == [
+        mocker.call('fake:fake')
+    ]
+    assert repository.memory_data_source.set.call_args_list == [
+        mocker.call('fake:not-found:fake', '1')
+    ]
+    assert not repository.memory_data_source.hmset.called
+
+
+@pytest.mark.asyncio
 async def test_should_get_from_fallback(repository, fake_entity):
     await repository.memory_data_source.delete('fake:fake')
     await repository.memory_data_source.delete('fake:not-found:fake')
