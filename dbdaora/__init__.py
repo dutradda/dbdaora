38bf2396f00d160f64aaac4ca8615f96b82975da
Diogo Dutra <diogo.albuquerque@luizalabs.com>
2020-05-27 04:15:07 -0300

    Bump version: 0.9.6 → 0.10.0

--- dbdaora/__init__.py
+++ dbdaora/__init__.py
@@ -1,7 +1,7 @@
 """Communicates with databases using repository pattern and service patterns"""
 
 
-__version__ = '0.9.6'
+__version__ = '0.10.0'
 
 from dbdaora.cache import CacheType, TTLDaoraCache
 from dbdaora.circuitbreaker import AsyncCircuitBreaker
